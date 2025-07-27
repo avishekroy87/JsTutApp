241d4c887da93ea07d0dce393f0fe86dcc5a382d
“Avishek <avishekroy87@gmail.com>
2025-07-27 19:28:06 +0530

    imgeenv

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,6 +1,19 @@
 import React from 'react'
 import './App.css'
 import Menu from './common/Menu'
+import { Card, CardMedia, IconButton, Stack, Typography } from '@mui/material'
+import PlayArrowRounded from '@mui/icons-material/PlayArrowRounded'
+import PauseRounded from '@mui/icons-material/PauseRounded'
+import ShuffleRoundedIcon from '@mui/icons-material/ShuffleRounded'
+import FastRewindRounded from '@mui/icons-material/FastRewindRounded'
+import FastForwardRounded from '@mui/icons-material/FastForwardRounded'
+import LoopRoundedIcon from '@mui/icons-material/LoopRounded'
+
+let paused = true
+const setPaused = (value: boolean) => {
+  paused = value
+}
+
 
 const items = [
   { label: 'Home', href: '/' },
@@ -11,22 +24,63 @@ const items = [
 
 const App: React.FC = () => {
   return (
-    <div className="App">
-      <header>
-        <Menu items={items} />
-      </header>
-      <main>
-        <header>Courses</header>
-          <div>
-              The courses are as follows:
-              <ul>
-                  <li>React</li>
-                  <li>Node</li>
-                  <li>Express</li>
-              </ul>
-          </div>
-      </main>
+   <Card
+  variant="outlined"
+  sx={{  p: 2,
+    width: { xs: '100%', sm: 'auto' },
+    display: 'flex',
+    flexDirection: { xs: 'column', sm: 'row' },
+    alignItems: 'center',
+    gap: 2,
+  }}
+>
+  <CardMedia
+    component="img"
+    width="100"
+    height="100"
+    alt="Contemplative Reptile album cover"
+    src={process.env.REACT_APP_IMG_URL}
+    sx={{    width: { xs: '100%', sm: 100 },
+    }}
+  />
+  <Stack direction="column" alignItems="center" spacing={1} useFlexGap>
+    <div>
+      <Typography color="text.primary" fontWeight="semiBold">
+        Contemplative Reptile
+      </Typography>
+      <Typography
+        variant="caption"
+        color="text.secondary"
+        fontWeight="medium"
+        textAlign="center"
+        sx={{ width: '100%' }}
+      >
+        Sounds of Nature
+      </Typography>
     </div>
+    <Stack direction="row" alignItems="center" spacing={1} useFlexGap>
+      <IconButton aria-label="Shuffle" disabled size="small">
+        <ShuffleRoundedIcon fontSize="small" />
+      </IconButton>
+      <IconButton aria-label="Fast rewind" disabled size="small">
+        <FastRewindRounded fontSize="small" />
+      </IconButton>
+      <IconButton
+        aria-label={paused ? 'Play music' : 'Pause music'}
+        onClick={() => setPaused((val: boolean) => !val)}
+        sx={{ mx: 1 }}
+      >
+        {paused ? <PlayArrowRounded /> : <PauseRounded />}
+      </IconButton>
+      <IconButton aria-label="Fast forward" disabled size="small">
+        <FastForwardRounded fontSize="small" />
+      </IconButton>
+      <IconButton aria-label="Loop music" disabled size="small">
+        <LoopRoundedIcon fontSize="small" />
+      </IconButton>
+    </Stack>
+  </Stack>
+</Card>
   )
 }
 
